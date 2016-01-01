5e466ad4069ac050283b448eed19bc8741677071
SocketPhys <SocketPhys@gmail.com>
2015-12-31 18:56:54 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var server = http.listen(9000, function(){
+  console.log('listening on *:3050');
+});
+// Start Binary.js server
+var binaryServer = BinaryServer({server: server});
+// Wait for new user connections
+binaryServer.on('connection', function(client){
+  // Stream a flower as a hello!
+  var file = fs.createReadStream(__dirname + '/adele.mp3');
+  client.send(file); 
+});
